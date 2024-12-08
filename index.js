2520e0fbccaf9b3274a761e4f5783cd5f5519d92
eddy <eddy.mwaniki.27@gmail.com>
2024-12-09 02:52:44 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching (drivers, search){
+ return drivers.filter(driver => driver.toLowerCase() === search.toLowerCase() )
+}
+
+function fuzzyMatch (drivers, search) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(search.toLowerCase()))
+}
+
+function matchName (drivers, search) {
+    return drivers.filter(driver => driver.name.toLowerCase()=== search.toLowerCase())
+}
